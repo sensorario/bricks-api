9292084920cc5fa8034e1b48395b6cb305873c21
Simone Gentili <sensorario@gmail.com>
2016-03-18 17:44:27 +0100

    Smal refactoring to improve the readability of code

--- src/Bricks/Factories/ResponseFactory.php
+++ src/Bricks/Factories/ResponseFactory.php
@@ -10,31 +10,6 @@ use Bricks\Response\Response;
 /** @todo inject a service that count resources */
 class ResponseFactory
 {
-    public function getStatistics()
-    {
-        return $this->getEmptyResponse()
-            ->withKeyValue('sets', count(file('app/data/bricks.objects.set')))
-            ->withKeyValue('insights', count(file('app/data/bricks.objects.insight')))
-            ->withKeyValue('shops', count(file('app/data/bricks.objects.shop')))
-            ->withLink('self', '/stats/');
-    }
-
-    private function getEmptyResponse()
-    {
-        return Response::createEmpty()
-            ->withLink('homepage', '/homepage/');
-    }
-
-    public function getInsight(Insight $insight)
-    {
-        return $this->getEmptyResponse()
-            ->withKeyValue('shop', $insight->getShop())
-            ->withKeyValue('set', $insight->getSet())
-            ->withKeyValue('value', $insight->getValue())
-            ->withKeyValue('update', $insight->getUpdate())
-            ->withLink('self', '/shop/' . $insight->getShop());
-    }
-
     public function getHomepage()
     {
         return $this->getEmptyResponse()
@@ -44,21 +19,43 @@ class ResponseFactory
             ->withLink('stats', '/stats/');
     }
 
+    private function getEmptyResponse()
+    {
+        return Response::createEmpty()
+            ->withLink('homepage', '/homepage/');
+    }
+
+    public function getStatistics()
+    {
+        return $this->getEmptyResponse()
+            ->withKeyValue('sets', count(file('app/data/bricks.objects.set')))
+            ->withKeyValue('insights', count(file('app/data/bricks.objects.insight')))
+            ->withKeyValue('shops', count(file('app/data/bricks.objects.shop')))
+            ->withLink('self', '/stats/');
+    }
+
+    public function getInsight(Insight $insight)
+    {
+        return $this->getEmptyResponse()
+            ->withKeyValue('shop', $insight->getShop())
+            ->withKeyValue('set', $insight->getSet())
+            ->withKeyValue('value', $insight->getValue())
+            ->withKeyValue('update', $insight->getUpdate())
+
+            ->withLink('self', '/insight/' . $item->getTimestamp())
+            ->withLink('collection', '/insights/')
+        ;
+    }
+
     public function getSet(Set $item)
     {
         return $this->getEmptyResponse()
             ->withKeyValue('code', $item->getCode())
             ->withKeyValue('update', $item->getUpdate())
-            ->withLink('self', '/set/' . $item->getCode())
-            ->withLink('collection', '/sets/');
-    }
 
-    public function getCollection($link, $collection, $links)
-    {
-        return $this->getEmptyResponse()
-            ->withKeyValue('collection', $collection)
-            ->withKeyValue('links', $links)
-            ->withLink('collection', $link);
+            ->withLink('self', '/set/' . $item->getCode())
+            ->withLink('collection', '/sets/')
+        ;
     }
 
     public function getShop(Shop $item)
@@ -68,6 +65,17 @@ class ResponseFactory
             ->withKeyValue('slug', $item->getSlug())
             ->withKeyValue('address', $item->getAddress())
             ->withKeyValue('update', $item->getUpdate())
-            ->withLink('self', '/shop/' . $item->getSlug());
+
+            ->withLink('self', '/insight/' . $item->getTimestamp())
+            ->withLink('collection', '/insights/')
+        ;
+    }
+
+    public function getCollection($link, $collection, $links)
+    {
+        return $this->getEmptyResponse()
+            ->withKeyValue('collection', $collection)
+            ->withKeyValue('links', $links)
+            ->withLink('collection', $link);
     }
 }
